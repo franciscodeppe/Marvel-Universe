b523950e7821fb9ac0e02ed4e0ada0520aaf2cef
francisco <francisco.deppe@gmail.com>
2017-07-25 21:16:51 -0700

    updates

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -47,6 +47,10 @@ app.get("/", function(req, res) {
 
 app.get("/add", function(req, res) {
   res.sendFile(path.join(__dirname, "add.html"));
+
+app.listen(PORT, function() {
+
+};
 });
 
 // Search for Specific Character (or all characters) - provides JSON
